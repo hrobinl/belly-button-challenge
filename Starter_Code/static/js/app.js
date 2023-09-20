704574c5967990561e582d0e0497d7f841e3082b
hrobinl <132854695+hrobinl@users.noreply.github.com>
2023-09-20 00:10:18 -0400

    Third Commit - Adjusting Code

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,6 +1,6 @@
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-// Demographic Info:
+// Show Demographic Info:
 function panelInfo(id) {
     d3.json(url).then(function (data) {
         let sampleData = data;
@@ -15,7 +15,7 @@ function panelInfo(id) {
     })
 };
 
-// Data Plots:
+// Show Plots:
 function Plots(id) {
     d3.json(url).then(function (data) {
         let sampleData = data;
@@ -60,7 +60,7 @@ function Plots(id) {
     })
 };
 
-// Build New Upon ID Change:
+// Build new upon ID change:
 function optionChanged(id) {
     Plots(id);
     panelInfo(id);
